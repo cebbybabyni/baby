1277d7d2fa87d03b571732329770dcd1215a08a4
cebbybabyni <jasonkerwinceballos@gmail.com>
2026-02-12 11:53:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ document.getElementById("yesBtn").onclick=()=>{
 // 📳 vibration
 if(navigator.vibrate) navigator.vibrate([200,100,200,100,400]);
 
-document.onclick = null;
+document.removeEventListener("click", heartBurst);
 
 document.body.innerHTML=`
 <div style="padding:30px">
